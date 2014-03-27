75bdc3214b57bd15b954e6d10d1e88c9f057ffe5
Greg <greg.knudsen09@gmail.com>
2014-03-27 00:36:03 -0400

    Add GPS 1.2 solution

--- 9_gps1.2/our_solution.js
+++ 9_gps1.2/our_solution.js
@@ -1,22 +1,48 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Greg Knudsen
+//  2. ...just me and teacher (Rich Vogt)
 
 
 // 1. "YOU SIGNED... WHO?!"
 
-
-
+kevinSpacey = {	// I had originally named these 'client1', 'client2', etc... Rich had suggested naming these clients 'kevinSpacey', etc... definitely a better idea
+    name: "Kevin Spacey",
+    age: 55,
+    quote: "I rule!"
+  }
 
 // 2. "Here they Come!"
 
+adamSandler = {
+  name: "Adam Sandler",
+  age: 47,
+  quote: "That's your home! Are you too good for your home?!"
+}  
 
-
-
+kristenBell = {
+  name: "Kristen Bell",
+  age: 33,
+  quote: "Do you wanna build a snowman?"
+}
+  
+jimCarrey = {
+  name: "Jim Carrey",
+  age: 52,
+  quote: "...So you're telling me there's a chance? YEAH!"
+}
 
 // 3. "TIME IS MONEY!"
+
+var clients = [kevinSpacey, adamSandler, kristenBell, jimCarrey]; // we decided to create an array of all the clients
+
+function Client(name, age, quote) {
+  this.name = name;
+  this.age = age;
+  this.quote = quote;
+  clients.push(this); // added this for more automation
+ }
 
 //YOUR CODE HERE!
 
@@ -25,13 +51,22 @@ shooterMcGavin.constructor === Client;
 shooterMcGavin.age === 48;
 shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
 
+// clients[4] = shooterMcGavin;
 
+// console.log(clients);
 
+// 4. "SHOW 'EM OFF!" 
 
+// this is original loop that Rich came up with
 
-// 4. "SHOW 'EM OFF!"
+// for(var i=0; i<clients.length; i++){
+//     console.log(clients[i].name + " " + clients[i].age + " " + clients[i].quote);
+// }
 
+// I just added a few things to make it a little more readable.
 
-
+for(var i=0; i<clients.length; i++){
+    console.log(clients[i].name + ", " + clients[i].age + ", \t" + '"'+ clients[i].quote +'"');
+}
 
 // ** BONUS **
